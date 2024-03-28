e3004abb2eeb441ae0f027491880c372f94519dc
Koko Adeola <kokoadeola84@gmail.com>
2024-03-28 01:34:01 -0700

    ALl Files

---
+++ 0x0D-preprocessor/0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: struct dog.
+ *
+ * Return: 0.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
---
+++ 0x0D-preprocessor/0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a README file for 0x0E. C - Structures, typedef project
